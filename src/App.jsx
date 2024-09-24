a7e5d5b2098229baefb09c09f42b45d65c998e0c
Patrick Binkert <patrick.binkert@outlook.de>
2024-09-24 14:09:22 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -56,8 +56,7 @@ function App() {
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
           </svg>
           <div>
-            Diese Seite befindet sich derzeit im Aufbau. Melde dich gern, wenn
-            dir Fehler auffallen!
+            Aufgrund eines unerwarteten Server-Fehlers sind derzeit einige Dienste nur eingeschränkt erreichbar.
           </div>
         </div>
         <BrowserRouter>
