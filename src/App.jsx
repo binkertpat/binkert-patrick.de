83a80bcdab401eeb9460df424eb6e5a1d0da01ca
Patrick Binkert <patrick.binkert@outlook.de>
2025-02-27 10:45:31 +0100

    added 404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import EducationMaterial from "./pages/EducationMaterial.jsx";
 import SelfHostedTools from "./pages/SelfHostedTools.jsx";
 import ExternalTools from "./pages/ExternalTools.jsx";
 import ExternalPrograms from "./pages/ExternalPrograms.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   const { _, dispatch } = useStateContext();
@@ -61,7 +62,7 @@ function App() {
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
           </svg> */}
           <div>
-            Alle Dienste sind wieder verfügbar! 😊🤝 [schauen wir mal wie lange.. 👀]
+            (fast) alle Dienste sind wieder verfügbar! 😊🤝 
           </div>
         </div>
         <BrowserRouter>
@@ -73,6 +74,7 @@ function App() {
             <Route path="/external-tools" element={<ExternalTools />} />
             <Route path="/external-programs" element={<ExternalPrograms />} />
             <Route path="/education" element={<EducationMaterial />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </div>
