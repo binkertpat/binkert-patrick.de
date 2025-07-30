0fc307930d23e950f2793be5c42374eecb3a1e93
Patrick Binkert <patrick.binkert@outlook.de>
2025-07-30 21:26:20 +0200

    added spinner

--- src/pages/SurfaceWin11UpgradeBooking.jsx
+++ src/pages/SurfaceWin11UpgradeBooking.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import ToolsCard from "../components/ToolsCard";
-import { SELF_HOSTED_TOOLS } from "../content/selfHostedTools";
+import Spinner from "../components/Spinner";
 
 const SurfaceWin11UpgradeBooking = () => {
   const [bookings, setBookings] = useState([]);
@@ -8,18 +7,18 @@ const SurfaceWin11UpgradeBooking = () => {
   const [optionState, updateOptionState] = useState({})
   const [addedBooking, setAddedBooking] = useState(false)
 
- const handleOptionChange = (e, userID) => {
-    updateOptionState({...optionState, ...{[userID] : e.target.value}})
-  } 
+  const handleOptionChange = (e, userID) => {
+    updateOptionState({ ...optionState, ...{ [userID]: e.target.value } })
+  }
 
   const handleSubmitButton = (e, userID) => {
-    if(optionState[userID] != undefined) {
+    if (optionState[userID] != undefined) {
       addBooking(userID, optionState[userID])
     }
   }
 
   const addBooking = async (userID, timeID) => {
-    const url = "https://binkert-patrick.de/api/addBooking.php?timeID="+timeID+"&userID="+userID;
+    const url = "https://binkert-patrick.de/api/addBooking.php?timeID=" + timeID + "&userID=" + userID;
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -66,10 +65,10 @@ const SurfaceWin11UpgradeBooking = () => {
   };
 
   const [count, setCount] = useState(0)
-   useEffect(() => {
-    const timer = setTimeout(() => setCount(count+1), 5000)
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 5000)
     return () => clearTimeout(timer)
-   }, [count])
+  }, [count])
 
   useEffect(() => {
     initFetching();
@@ -78,63 +77,70 @@ const SurfaceWin11UpgradeBooking = () => {
   return (
     <>
       <h1 className="display-5 fw-bold lh-1 mb-3">Zeitslot buchen</h1>
-      {bookings != [] && freeTimeslots != [] && (
+
+      {bookings.length == 0 || freeTimeslots.length == 0 && (
+        <div className="d-flex justify-content-center">
+          <Spinner />
+        </div>
+      )}
+
+      {bookings.length > 0 && freeTimeslots.length > 0 && (
         <div className="table-responsive">
-        <table className="table table-striped align-middle text-center">
-          <thead>
-            <tr>
-              {/* <th scope="col">#</th> */}
-              <th scope="col">Initialen</th>
-              <th scope="col">Inventarnummer</th>
-              <th scope="col">Zeitslot</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.values(bookings).map((item, i) => {
-              return (
-                <tr key={i}>
-                  {/* <th scope="row">#{item.userID}</th> */}
-                  <td>{item.shortText}</td>
-                  <td>{item.inventaryNumber}</td>
-                  {item.bookedTimeSlot != undefined && (
-                    <td>{item.timeSlot}</td>
-                  )}
-                  {item.bookedTimeSlot == undefined && (
-                    <td>
-                      <div className="d-grid gap-2 align-items-center">
-                        <select 
-                          style={{minWidth : '250px'}}
-                          key={item.userID}
-                          className="form-select" 
-                          aria-label="Zeitslot-Selector" 
-                          value={optionState[item.userID]} 
-                          placeholder="Wähle deinen Zeitslot!"
-                          onChange={(e) => handleOptionChange(e, item.userID)}
-                        >
-                          <option>Wähle deinen Tag!</option>
-                          {Object.values(freeTimeslots).map((time) => {
-                            return (
-                              <option 
-                                key={time.timeID}
-                                value={time.timeID}>{time.timeSlot}</option>
-                            )    
-                          })}
-                        </select>
-                        <button 
-                          type="button" 
-                          className="btn btn-success ms-2"
-                          onClick={(e) => handleSubmitButton(e, item.userID)}
-                        >
-                          Bestätigen
-                        </button>
-                      </div>
-                    </td>
-                  )}
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+          <table className="table table-striped align-middle text-center">
+            <thead>
+              <tr>
+                {/* <th scope="col">#</th> */}
+                <th scope="col">Initialen</th>
+                <th scope="col">Inventarnummer</th>
+                <th scope="col">Zeitslot</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.values(bookings).map((item, i) => {
+                return (
+                  <tr key={i}>
+                    {/* <th scope="row">#{item.userID}</th> */}
+                    <td>{item.shortText}</td>
+                    <td>{item.inventaryNumber}</td>
+                    {item.bookedTimeSlot != undefined && (
+                      <td>{item.timeSlot}</td>
+                    )}
+                    {item.bookedTimeSlot == undefined && (
+                      <td>
+                        <div className="d-grid gap-2 align-items-center">
+                          <select
+                            style={{ minWidth: '250px' }}
+                            key={item.userID}
+                            className="form-select"
+                            aria-label="Zeitslot-Selector"
+                            value={optionState[item.userID]}
+                            placeholder="Wähle deinen Zeitslot!"
+                            onChange={(e) => handleOptionChange(e, item.userID)}
+                          >
+                            <option>Wähle deinen Tag!</option>
+                            {Object.values(freeTimeslots).map((time) => {
+                              return (
+                                <option
+                                  key={time.timeID}
+                                  value={time.timeID}>{time.timeSlot}</option>
+                              )
+                            })}
+                          </select>
+                          <button
+                            type="button"
+                            className="btn btn-success ms-2"
+                            onClick={(e) => handleSubmitButton(e, item.userID)}
+                          >
+                            Bestätigen
+                          </button>
+                        </div>
+                      </td>
+                    )}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
         </div>
       )}
     </>
